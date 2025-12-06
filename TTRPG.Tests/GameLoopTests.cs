5a807465ec2e1fdc79d1daa484a3f3b1a917588a
HrVanker <mr.vanker@gmail.com>
2025-12-06 09:20:37 -0800

    df

--- TTRPG.Tests/GameLoopTests.cs
+++ TTRPG.Tests/GameLoopTests.cs
@@ -77,5 +77,52 @@ namespace TTRPG.Tests
             network.Stop();
             World.Destroy(world);
         }
+        [Fact]
+        public void Move_ShouldBeBlocked_WhenWallExists()
+        {
+            // Arrange
+            var world = World.Create();
+            var network = new ServerNetworkService();
+            network.Start(0);
+            network.SetWorld(world);
+
+            // Setup Map with a wall at (1,0)
+            var mapService = new MapService();
+            // We can write a temp file or just subclass/mock MapService if we made it virtual.
+            // For simplicity, we'll write a temp TMX again.
+            string mapXml = @"<?xml version=""1.0"" encoding=""UTF-8""?>
+            <map version=""1.10"" width=""5"" height=""5"" tilewidth=""16"" tileheight=""16"">
+            <layer id=""1"" name=""Collisions"" width=""5"" height=""5"">
+            <data encoding=""csv"">
+            0,1,0,0,0,
+            0,0,0,0,0,
+            0,0,0,0,0,
+            0,0,0,0,0,
+            0,0,0,0,0
+            </data>
+            </layer>
+            </map>";
+            string tempPath = System.IO.Path.GetTempFileName();
+            System.IO.File.WriteAllText(tempPath, mapXml);
+            mapService.LoadMap(tempPath);
+
+            var notifications = new NotificationService(network, world);
+            var gameLoop = new GameLoopService(network, world, notifications, mapService);
+
+            // Player at (0,0)
+            var entity = world.Create(new Position { X = 0, Y = 0 }, new Zone { Id = "Zone_A" });
+
+            // Act: Try to move Right to (1,0) -> Should hit Wall
+            network.OnPlayerInput?.Invoke(entity, MoveDirection.Right);
+
+            // Assert
+            var pos = world.Get<Position>(entity);
+            Assert.Equal(0, pos.X); // Should NOT have changed to 1
+
+            // Cleanup
+            network.Stop();
+            World.Destroy(world);
+            System.IO.File.Delete(tempPath);
+        }
     }
 }
